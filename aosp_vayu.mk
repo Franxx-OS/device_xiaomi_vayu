d377b88de365f0a6ec633fb4ba9c116fc63c4fa8
hirero765 <forpayment169@gmail.com>
2022-10-16 00:06:37 +0530

    Initial Franxx Upbring
    Signed-off-by: hirero765 <forpayment169@gmail.com>

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common configurations from Pixel Experience 
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit common configurations from Franxx vendor 
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 PRODUCT_NAME := aosp_vayu
 PRODUCT_DEVICE := vayu
@@ -26,3 +26,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=vayu_global \
     PRODUCT_MODEL=M2102J20SI
+
+# Franxx Config
+FRANXX_MAINTAINER := kickout_765
+FRANXX_BUILD_TYPE := official
+
+BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.5.0.SJUMIXM:user/release-keys
